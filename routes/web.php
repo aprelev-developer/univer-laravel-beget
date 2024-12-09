9f1a9d2d42f8d33c71556493f732fc9eed3ad70c
aprelev-developer <aprelev.a.developer@yandex.ru>
2024-12-10 01:32:06 +0300

    Повторные изменения

--- routes/web.php
+++ routes/web.php
@@ -74,7 +74,7 @@ Route::middleware(['auth', 'role:expert'])->prefix('expert')->name('expert.')->g
 });
 
 // Вуз
-Route::middleware(['auth', 'role:university'])->prefix('university')->name('university.')->group(function () {
+Route::middleware(['auth', 'role:university'])->group(function () {
     Route::get('/', [UniversityFormController::class, 'index'])->name('dashboard');
     Route::get('/university/form', [UniversityFormController::class, 'showForm'])->name('university.form');
     Route::post('/university/form', [UniversityFormController::class, 'submitForm'])->name('university.submitForm');
