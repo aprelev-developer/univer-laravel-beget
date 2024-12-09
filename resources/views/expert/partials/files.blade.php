500c3acb4b426824d64af33883d8bba4489cfda9
aprelev-developer <aprelev.a.developer@yandex.ru>
2024-12-09 23:28:16 +0300

    +добавление отображени файлов для экспертов при проверке вузов

---
+++ resources/views/expert/partials/files.blade.php
@@ -0,0 +1,21 @@
+@php
+// $data - данные поля (строка или массив)
+// $name - имя поля file_...
+@endphp
+
+@if(isset($data))
+    @if(is_array($data))
+        <p>Загруженные файлы:</p>
+        <ul>
+            @foreach($data as $file)
+                <li><a href="{{ asset('storage/' . $file) }}" target="_blank">Скачать</a></li>
+            @endforeach
+        </ul>
+    @else
+        <p>Загруженный файл: <a href="{{ asset('storage/' . $data) }}" target="_blank">Скачать</a></p>
+    @endif
+@endif
+<div class="form-group">
+    <label>Загрузить файлы (можно несколько)</label>
+    <input type="file" name="{{ $name }}[]" class="form-control-file" multiple>
+</div>
