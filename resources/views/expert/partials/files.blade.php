2613c7d456fb359e0bb31e2be0932394f9058bc4
aprelev-developer <aprelev.a.developer@yandex.ru>
2024-12-12 01:49:42 +0300

    Поправил отображение пустых значений в карточке редактирования эксперта

--- resources/views/expert/partials/files.blade.php
+++ resources/views/expert/partials/files.blade.php
@@ -4,16 +4,19 @@
 @endphp
 
 @if(isset($data))
-    @if(is_array($data))
-        <p>Загруженные файлы:</p>
-        <ul>
-            @foreach($data as $file)
-                <li><a href="{{ asset('storage/' . $file) }}" target="_blank">Скачать</a></li>
-            @endforeach
-        </ul>
-    @else
-        <p>Загруженный файл: <a href="{{ asset('storage/' . $data) }}" target="_blank">Скачать</a></p>
+@if(is_array($data))
+<p>Загруженные файлы:</p>
+<ul>
+    @foreach($data as $file)
+    @if(!empty($file))
+    <li><a href="{{ asset('storage/' . $file) }}" target="_blank">Скачать</a></li>
     @endif
+    @endforeach
+
+</ul>
+@else
+<p>Загруженный файл: <a href="{{ asset('storage/' . $data) }}" target="_blank">Скачать</a></p>
+@endif
 @endif
 <div class="form-group">
     <label>Загрузить файлы (можно несколько)</label>
